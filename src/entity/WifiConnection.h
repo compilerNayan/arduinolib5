127011f8681852250764b30bdba633fa8a4ac5ca
Nayan Kurude <nkurude@atlassian.com>
2026-01-09 12:32:38 +0530

    djkasjk

---
+++ src/entity/WifiConnection.h
@@ -0,0 +1,17 @@
+#ifndef WIFICONNECTION_H
+#define WIFICONNECTION_H
+
+#include <StandardDefines.h>
+
+/// @Entity
+class WifiConnection {
+
+    /// @Id
+    /// @NotNull
+    Public optional<int> id;
+
+    Public optional<StdString> lastConnectedSsid;
+};
+
+#endif
+
---
+++ src/repository/WifiConnectionRepository.h
@@ -0,0 +1,16 @@
+#ifndef _WIFI_CONNECTION_REPOSITORY_H_
+#define _WIFI_CONNECTION_REPOSITORY_H_
+
+#include <StandardDefines.h>
+#include "CpaRepository.h"
+#include "../entity/WifiConnection.h"
+
+/// @Repository
+DefineStandardPointers(WifiConnectionRepository)
+class WifiConnectionRepository : public CpaRepository<WifiConnection, int> {
+    Public Virtual ~WifiConnectionRepository() = default;
+    
+};
+
+#endif // _WIFI_CONNECTION_REPOSITORY_H_
+
