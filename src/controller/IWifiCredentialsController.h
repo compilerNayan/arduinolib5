99af00d6ab30be92db99ba5a19c8081117e2d2a6
Nayan Kurude <nkurude@atlassian.com>
2026-01-09 15:27:42 +0530

    kdjasjk

---
+++ src/controller/IWifiCredentialsController.h
@@ -0,0 +1,31 @@
+#ifndef IWIFI_CREDENTIALS_CONTROLLER_H
+#define IWIFI_CREDENTIALS_CONTROLLER_H
+
+#include <StandardDefines.h>
+#include "../entity/WifiCredentials.h"
+
+DefineStandardPointers(IWifiCredentialsController)
+class IWifiCredentialsController {
+    Public Virtual ~IWifiCredentialsController() = default;
+
+    // Create - Add new WiFi credentials
+    Public Virtual WifiCredentials CreateWifiCredentials(WifiCredentials credentials) = 0;
+
+    // Read - Get WiFi credentials by SSID
+    Public Virtual optional<WifiCredentials> GetWifiCredentials(const StdString& ssid) = 0;
+
+    // Read All - Get all WiFi credentials
+    Public Virtual vector<WifiCredentials> GetAllWifiCredentials() = 0;
+
+    // Update - Update existing WiFi credentials
+    Public Virtual WifiCredentials UpdateWifiCredentials(WifiCredentials credentials) = 0;
+
+    // Delete - Delete WiFi credentials by SSID
+    Public Virtual Void DeleteWifiCredentials(const StdString& ssid) = 0;
+
+    // Get last connected WiFi
+    Public Virtual optional<WifiCredentials> GetLastConnectedWifi() = 0;
+};
+
+#endif // IWIFI_CREDENTIALS_CONTROLLER_H
+
--- src/controller/WifiCredentialsController.h
+++ src/controller/WifiCredentialsController.h
@@ -2,50 +2,51 @@
 #define WIFI_CREDENTIALS_CONTROLLER_H
 
 #include <StandardDefines.h>
+#include "IWifiCredentialsController.h"
 #include "../service/IWifiService.h"
 #include "../entity/WifiCredentials.h"
 
 /// @RestController
 /// @RequestMapping("/wifi-credentials")
 /// @Component
-class WifiCredentialsController {
+class WifiCredentialsController : public IWifiCredentialsController {
     Public:
         /// @Autowired
         IWifiServicePtr wifiService;
 
         // Create - Add new WiFi credentials
         /// @PostMapping("")
-        WifiCredentials CreateWifiCredentials(WifiCredentials credentials) {
+        Public Virtual WifiCredentials CreateWifiCredentials(WifiCredentials credentials) override {
             return wifiService->AddWifiCredentials(credentials);
         }
 
         // Read - Get WiFi credentials by SSID
         /// @GetMapping("/{ssid}")
-        optional<WifiCredentials> GetWifiCredentials(const StdString& ssid) {
+        Public Virtual optional<WifiCredentials> GetWifiCredentials(const StdString& ssid) override {
             return wifiService->GetWifiCredentials(ssid);
         }
 
         // Read All - Get all WiFi credentials
         /// @GetMapping("")
-        vector<WifiCredentials> GetAllWifiCredentials() {
+        Public Virtual vector<WifiCredentials> GetAllWifiCredentials() override {
             return wifiService->GetAllWifiCredentials();
         }
 
         // Update - Update existing WiFi credentials
         /// @PutMapping("")
-        WifiCredentials UpdateWifiCredentials(WifiCredentials credentials) {
+        Public Virtual WifiCredentials UpdateWifiCredentials(WifiCredentials credentials) override {
             return wifiService->UpdateWifiCredentials(credentials);
         }
 
         // Delete - Delete WiFi credentials by SSID
         /// @DeleteMapping("/{ssid}")
-        Void DeleteWifiCredentials(const StdString& ssid) {
+        Public Virtual Void DeleteWifiCredentials(const StdString& ssid) override {
             wifiService->DeleteWifiCredentials(ssid);
         }
 
         // Get last connected WiFi
         /// @GetMapping("/last-connected")
-        optional<WifiCredentials> GetLastConnectedWifi() {
+        Public Virtual optional<WifiCredentials> GetLastConnectedWifi() override {
             return wifiService->GetLastConnectedWifi();
         }
 };
