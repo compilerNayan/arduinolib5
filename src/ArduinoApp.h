70603d0e2da797ce54ad191e30694367fbc585d3
Nayan Kurude <nkurude@atlassian.com>
2026-01-15 13:56:15 +0530

    jkjjk

---
+++ src/ArduinoApp.h
@@ -0,0 +1,71 @@
+#ifndef ARDUINOAPP_H
+#define ARDUINOAPP_H
+
+#include <StandardDefines.h>
+#include "IArduinoApp.h"
+#include "INetworkManager.h"
+#include "ISpringBootCppApp.h"
+
+#ifdef ARDUINO
+#include <Arduino.h>
+// Define print macros for Arduino
+#define std_print(x) Serial.print(x)
+#define std_println(x) Serial.println(x)
+#endif
+
+/* @Component */
+class ArduinoApp : public IArduinoApp {
+    Public Virtual ~ArduinoApp() = default;
+
+    /* @Autowired */
+    Private INetworkManagerPtr networkManager;
+
+    /* @Autowired */
+    Private ISpringBootCppAppPtr springBootCppApp;
+
+    Public Virtual Bool StartApp() override {
+        // First connect to network
+        if (networkManager != nullptr) {
+            networkManager->ConnectNetwork();
+        }
+        
+        // Then start the Spring Boot application
+        if (springBootCppApp != nullptr) {
+            return springBootCppApp->StartApp();
+        }
+        
+        return false;
+    }
+
+    Public Virtual Void StopApp() override {
+        // First disconnect from network
+        if (networkManager != nullptr) {
+            networkManager->DisconnectNetwork();
+        }
+        
+        // Then stop the Spring Boot application
+        if (springBootCppApp != nullptr) {
+            springBootCppApp->StopApp();
+        }
+    }
+
+    Public Virtual Bool RestartApp() override {
+        // Stop the application (calls StopApp of this class)
+        StopApp();
+        
+        // Start the application (calls StartApp of this class)
+        Bool result = StartApp();
+        
+        return result;
+    }
+
+    Public Virtual Void ListenToRequest() override {
+        // Delegate to Spring Boot application's ListenToRequest
+        if (springBootCppApp != nullptr) {
+            springBootCppApp->ListenToRequest();
+        }
+    }
+};
+
+#endif // ARDUINOAPP_H
+
---
+++ src/IArduinoApp.h
@@ -0,0 +1,38 @@
+#ifndef IARDUINOAPP_H
+#define IARDUINOAPP_H
+
+#include <StandardDefines.h>
+
+DefineStandardPointers(IArduinoApp)
+class IArduinoApp {
+    Public Virtual ~IArduinoApp() = default;
+
+    /**
+     * @brief Starts the Arduino application
+     * Connects to network and starts the Spring Boot application
+     * @return true if application started successfully, false otherwise
+     */
+    Public Virtual Bool StartApp() = 0;
+
+    /**
+     * @brief Stops the Arduino application
+     * Disconnects from network and stops the Spring Boot application
+     */
+    Public Virtual Void StopApp() = 0;
+
+    /**
+     * @brief Restarts the Arduino application
+     * Stops and then starts the application
+     * @return true if application restarted successfully, false otherwise
+     */
+    Public Virtual Bool RestartApp() = 0;
+
+    /**
+     * @brief Listens for and processes HTTP requests
+     * Delegates to Spring Boot application's ListenToRequest method
+     */
+    Public Virtual Void ListenToRequest() = 0;
+};
+
+#endif // IARDUINOAPP_H
+
